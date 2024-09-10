81830a0e71d3c10c0f746a40292472e488311d69
bain-liao <liao.bain@mths.ca>
2024-09-10 15:20:52 +0000

    changed to correct comment

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,10 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Bain Liao
+Created on: Sep 2024
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.scroll("Hello, Bain Liao!")
